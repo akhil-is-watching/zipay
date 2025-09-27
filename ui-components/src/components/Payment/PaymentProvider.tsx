ee7d2f55b385158c81356414169e813ffa7a6d44
Shiyas Mohammed <shiyasmohd.official@gmail.com>
2025-09-28 05:01:44 +0530

    update chains

--- ui-components/src/components/Payment/PaymentProvider.tsx
+++ ui-components/src/components/Payment/PaymentProvider.tsx
@@ -3,13 +3,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { base } from 'wagmi/chains';
+import { sepolia } from 'wagmi/chains';
 import '@rainbow-me/rainbowkit/styles.css';
 
 const config = getDefaultConfig({
   appName: 'Zipay',
   projectId: 'YOUR_PROJECT_ID',
-  chains: [base],
+  chains: [sepolia],
   ssr: false,
 });
 
--- ui-components/src/lib/const.ts
+++ ui-components/src/lib/const.ts
@@ -1,12 +1,12 @@
 export const CHAINS = {
-  monad: {
-    chainId: 10143,
-    usdc: "0x85f754abfD3b82158E2925f877f0b201187d3a3c",
-    rpc: "https://monad-testnet.drpc.org",
-  },
-  base: {
-    chainId: 8453,
-    usdc: "0xd2a541B282eb40707f00fC01cfA716EB8e035bDE",
-    rpc: "https://mainnet.base.org",
-  },
+    monad: {
+        chainId: 10143,
+        usdc: "0x85f754abfD3b82158E2925f877f0b201187d3a3c",
+        rpc: "https://monad-testnet.drpc.org",
+    },
+    sepolia: {
+        chainId: 11155111,
+        usdc: "0xAEE37f2E7BE54fd0B96B780BEC7Eb92BD2db9F85",
+        rpc: "https://sepolia.drpc.org",
+    },
 };
