2384d4a7b3528bc1b345addf9040769497b0132d
unniznd <au6695@gmail.com>
2025-09-27 15:02:00 +0530

    fixed import issue and added calling buildEVMOrder

--- resolver/src/routes/resolver.ts
+++ resolver/src/routes/resolver.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import { RelayerService } from '@/services/RelayerService';
+import { RelayerService } from '../services/RelayerService';
 
 const router = Router();
 const relayerService = new RelayerService();
 
 // POST /createSwap
-router.post('/createSwap', async (req, res) => {});
+router.post('/createSwap', async (req, res) => {
+  const order = relayerService.buildEvmSwapOrder(req.body);
+  res.json({ order },)
+});
 
 // POST /executeSwap
 router.post('/executeSwap', async (req, res) => {});
