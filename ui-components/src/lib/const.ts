b9ff6096d171cf6f3b2c6bc7cd550743dd158f48
Hashir <mail2hashirpm@gmail.com>
2025-09-27 16:02:45 +0530

    add usdc base contract

--- ui-components/src/lib/const.ts
+++ ui-components/src/lib/const.ts
@@ -1,6 +1,10 @@
 export const CHAINS = {
-    monad: {
-        chainId: 10143,
-        usdc: "0x85f754abfD3b82158E2925f877f0b201187d3a3c",
-    },
-}
+  monad: {
+    chainId: 10143,
+    usdc: "0x85f754abfD3b82158E2925f877f0b201187d3a3c",
+  },
+  base: {
+    chainId: 8453,
+    usdc: "0xd2a541B282eb40707f00fC01cfA716EB8e035bDE",
+  },
+};
