2b22099b2463ee431475185e3b632040333f540c
unniznd <au6695@gmail.com>
2025-09-28 06:54:37 +0530

    fixed cors error

--- resolver/src/index.ts
+++ resolver/src/index.ts
@@ -1,7 +1,24 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { apiRouter } from './routes';
 
 const app = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  // Allow all cross-origin requests.
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    req.header('Access-Control-Request-Headers') ?? 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+    return;
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use('/api', apiRouter);
